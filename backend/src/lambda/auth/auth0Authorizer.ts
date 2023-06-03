6909c2011a50f912874ae302cd997de73d2e5220
larry <larrytientcheu1@gmail.com>
2023-06-03 02:59:40 +0200

    authorization

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -7,12 +7,15 @@ import Axios from 'axios'
 import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
+const SigningKeyNotFoundError = require('./error')
 const logger = createLogger('auth')
+let cachedCertificate: string
 
 // TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+const jwksUrl = 'https://dev-aktou0ou56nwf6pg.us.auth0.com/.well-known/jwks.json'
+
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -61,7 +64,8 @@ async function verifyToken(authHeader: string): Promise<JwtPayload> {
   // TODO: Implement token verification
   // You should implement it similarly to how it was implemented for the exercise for the lesson 5
   // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+  const certificate = await getCertificate(jwt.header.kid)
+  return verify(token, certificate, { algorithms: ['RS256'] }) as JwtPayload
 }
 
 function getToken(authHeader: string): string {
@@ -74,4 +78,25 @@ function getToken(authHeader: string): string {
   const token = split[1]
 
   return token
+}
+
+async function getCertificate(kid: string): Promise<string> {
+  try {
+
+    if (cachedCertificate) return cachedCertificate
+    logger.info(`Fetching certificate`)
+    const jwks = await Axios.get(jwksUrl)
+    const key = jwks.data.keys.find(k => k.kid === kid)
+  
+    if (!key) {
+      throw new SigningKeyNotFoundError(`Unable to find a signing key that matches '${kid}'`)}
+    
+    let certificate = `-----BEGIN CERTIFICATE-----\n${key.x5c[0]}\n-----END CERTIFICATE-----`
+    cachedCertificate = certificate
+    return cachedCertificate
+  
+  } catch (error) {
+    logger.error('Could not get Auth0 certificat', {error:error.message})
+  }
+    
 }
