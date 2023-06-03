f6bb96271197fb91ea1a120a59fd7d8885bd3678
larry <larrytientcheu1@gmail.com>
2023-06-03 23:33:18 +0200

    GetTodos for users

--- backend/src/dataLayer/todosAcess.ts
+++ backend/src/dataLayer/todosAcess.ts
@@ -12,5 +12,23 @@ const logger = createLogger('TodosAccess')
 // TODO: Implement the dataLayer logic
 
 export class TodosAcess {
-    
+
+    constructor(
+        
+        private readonly s3Bucket = process.env.ATTACHMENT_S3_Bucket,
+        private readonly todosTable = process.env.TODOS_TABLE,
+        private readonly docClient: DocumentClient = new XAWS.DynamoDB.DocumentClient()
+
+    ){}
+
+    async getTodosForUser(userId: string): Promise<TodoItem[]>{
+        logger.info('Getting Todo\'s')
+
+        let params = {
+            Tablename: this.todosTable,
+            KeyConditionExpression: "userId = :userId",
+            ExpressionAttributeValues: {":userId": userId}
+        }
+    }
+
 }
