439d6dfeabfff9fb16508c05586bbd72965b9ed0
larry <larrytientcheu1@gmail.com>
2023-06-04 19:21:25 +0200

    heap limit error

--- backend/src/dataLayer/attachmentUtils.ts
+++ backend/src/dataLayer/attachmentUtils.ts
@@ -10,7 +10,7 @@ const logger = createLogger('attachmentUtils')
 export class AttachmentUtils{
     constructor(
         private readonly s3 = new XAWS.S3({ signatureVersion: 'v4' }),
-        private readonly bucketName = process.env.ATTACHMENT_S3,
+        private readonly bucketName = process.env.ATTACHMENT_S3_BUCKET,
         private readonly signedUrlExpiration = process.env.SIGNED_URL_EXPIRATION
     ){}
 
